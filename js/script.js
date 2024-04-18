f23a5e16a44ac3cecbe758a2354ba6005040d010
William Brown <140029632+brogment@users.noreply.github.com>
2024-04-17 19:27:43 -0500

    All operations working

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let num1 = 0;
 let num2 = 0;
 let operator = '';
 let displayVal = '';
-
+document.getElementById('display').textContent = 0;
 
 function operate (num1, operator, num2) {
     if(operator=='+'){
@@ -16,6 +16,7 @@ function operate (num1, operator, num2) {
         return divide(num1, num2);
     } 
 }
+
 function add (num1, num2){
     return num1 + num2;
 }
@@ -23,28 +24,32 @@ function subtrack (num1, num2){
     return num1 - num2;
 }
 function multiply (num1, num2){
-  
     return num1 * num2;
 }
 function divide (num1, num2){
-
     return num1 / num2;
 }
 
 
 function depressKey(e){
-    const keyButton  = document.querySelector(`[data-key="${e.keyCode}"]`);
-    keyButton.classList.add('pressed');
     
+    const keyButton  = document.querySelector(`[data-key="${e.keyCode}"]`);
+    keyButton.classList.add('pressed');  
+
     displayVal += e.key;
-
-    console.log(`Num1: ${num1}`)
-    console.log(`DisplayVal: ${displayVal}`)
-
     document.getElementById('display').textContent = displayVal;
-    if(e.key==='+' || e.key==='-' || e.key==='*' || e.key==='/'){
-        operator = e.key;
 
+    if(e.key==='+' || e.key==='-' || e.key==='*' || e.key==='/'){
+        
+        if(operator){ 
+            num1=operate(num1, operator, Number(displayVal.slice(0, -1)))
+            operator = e.key;
+            displayVal = '';
+            document.getElementById('display').textContent = num1;
+
+        } else {
+
+        operator = e.key;
 
         if(num1 == 0){
             num1=Number(displayVal.slice(0, -1));
@@ -52,24 +57,23 @@ function depressKey(e){
 
         displayVal = '';
         document.getElementById('display').textContent = num1;
-
-        console.log(`Num1: ${num1}`)
-        console.log(`DisplayVal: ${displayVal}`)
+        }
     }
 
     if(e.key==='='){
         if(num1 == 0){
             num1=Number(displayVal.slice(0, -1));
-        } else {num1 = operate(num1, operator, Number(displayVal.slice(0, -1)))};
+        } else if(operator === ''){
+            num1=Number(displayVal.slice(0, -1));
+        } else {
+            num1 = operate(num1, operator, Number(displayVal.slice(0, -1)))
+        }
+        
         operator = '';
-        
-        displayVal = '';
-        document.getElementById('display').textContent = num1;
-        console.log(`Num1: ${num1}`)
-        console.log(`DisplayVal: ${displayVal}`)
-        
+        displayVal = num1;
+        num1=0;
+        document.getElementById('display').textContent = displayVal;     
     }
-    
 }
 
 //only for press effect
@@ -82,3 +86,5 @@ function unDepressKey(e){
 
 window.addEventListener('keydown', depressKey);
 window.addEventListener('keyup', unDepressKey);
+window.addEventListener('mousedown', depressKey);
+window.addEventListener('mouseup', unDepressKey);
